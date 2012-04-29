6d8eba19a4b0ec8ece5ec26ff26ad32299c050b3
Chris McMacken <chris.mcmacken@gmail.com>
2012-04-29 18:07:24 -0500

    Converting to tabs

--- mac/ColorClock/ColorClock/ColorClockView.m
+++ mac/ColorClock/ColorClock/ColorClockView.m
@@ -11,54 +11,54 @@
 @implementation ColorClockView
 
 - (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview {
-    self = [super initWithFrame:frame isPreview:isPreview];
-    if (self) {
-        [self setAnimationTimeInterval:1/30.0];
-    }
-    return self;
+	self = [super initWithFrame:frame isPreview:isPreview];
+	if (self) {
+		[self setAnimationTimeInterval:1/30.0];
+	}
+	return self;
 }
 
 - (void)startAnimation {
-    [super startAnimation];
+	[super startAnimation];
 }
 
 - (void)stopAnimation {
-    [super stopAnimation];
+	[super stopAnimation];
 }
 
 - (void)drawRect:(NSRect)rect {
 	// Get a color and hex string for the current time
 	NSDate *date = [NSDate date];
 	NSColor *color = [self getColorForTime:date];
-    NSString *time;
-    CGFloat fontSize;
-    const BASE_FONT_SIZE = 20;
-    NSDateFormatter *timeFormat = [NSDateFormatter new];
-    NSFont *font;
-    NSColor *fontColor = [NSColor colorWithCalibratedRed:1.0 green:1.0 blue:1.0 alpha:0.6];
-    [timeFormat setDateFormat:@"H:mm:ss"];
-    time = [timeFormat stringFromDate:date];
+	NSString *time;
+	CGFloat fontSize;
+	const int BASE_FONT_SIZE = 20;
+	NSDateFormatter *timeFormat = [NSDateFormatter new];
+	NSFont *font;
+	NSColor *fontColor = [NSColor colorWithCalibratedRed:1.0 green:1.0 blue:1.0 alpha:0.6];
+	[timeFormat setDateFormat:@"H:mm:ss"];
+	time = [timeFormat stringFromDate:date];
 	
 	[color set];
     
-    NSSize screenSize = rect.size;
+	NSSize screenSize = rect.size;
     
-    //set the font size based on screen size
-    fontSize = BASE_FONT_SIZE * (screenSize.width / 200);
+	//set the font size based on screen size
+	fontSize = BASE_FONT_SIZE * (screenSize.width / 200);
     
 	[NSBezierPath fillRect:rect];	
 
-    font = [NSFont fontWithName:@"Verdana" size:fontSize];
+	font = [NSFont fontWithName:@"Verdana" size:fontSize];
 	NSMutableParagraphStyle* style = [NSMutableParagraphStyle new];
 	[style setAlignment:NSCenterTextAlignment];
 
-    NSArray *values = [NSArray arrayWithObjects:font, style, fontColor, nil];
-    NSArray *keys = [NSArray arrayWithObjects:NSFontAttributeName, NSParagraphStyleAttributeName, NSForegroundColorAttributeName, nil];
+	NSArray *values = [NSArray arrayWithObjects:font, style, fontColor, nil];
+	NSArray *keys = [NSArray arrayWithObjects:NSFontAttributeName, NSParagraphStyleAttributeName, NSForegroundColorAttributeName, nil];
     
 	NSDictionary *attr = [NSDictionary dictionaryWithObjects:values forKeys:keys];
-    NSSize stringSize = [time sizeWithAttributes:attr];
-    CGFloat y = ((screenSize.height / 2) -  (stringSize.height / 2));
-    CGFloat x = ((screenSize.width / 2) - (stringSize.width / 2));
+	NSSize stringSize = [time sizeWithAttributes:attr];
+	CGFloat y = ((screenSize.height / 2) -  (stringSize.height / 2));
+	CGFloat x = ((screenSize.width / 2) - (stringSize.width / 2));
     
 	[time drawAtPoint:NSMakePoint(x, y) withAttributes:attr];
 	
@@ -66,8 +66,8 @@
 }
 
 - (void)animateOneFrame {    
-    [self setNeedsDisplay:YES];
-    return;
+	[self setNeedsDisplay:YES];
+	return;
 }
 
 -(NSString*) getHexColorForTime:(NSDate*)date {
@@ -90,7 +90,7 @@
 	[formatter setDateFormat:@"H"];
 	float hours = [[formatter stringFromDate:date] floatValue];
     
-    firstDigit = round(((hours + (minutes / 60.0)) / 24.0) * 15);
+	firstDigit = round(((hours + (minutes / 60.0)) / 24.0) * 15);
 	secondDigit = (hours + (minutes / 60.0)) / 8.0;
 	tmpNumber = floor(secondDigit);
 	secondDigit = round((secondDigit - tmpNumber) * 15);
@@ -114,31 +114,31 @@
 - (NSColor*)getColorForTime:(NSDate*)date {
 	NSString *hex = [self getHexColorForTime:date];
     
-    unsigned int tmpRed, tmpGreen, tmpBlue;
-    float red, green, blue;
+	unsigned int tmpRed, tmpGreen, tmpBlue;
+	float red, green, blue;
     
-    NSScanner *scannerRed = [NSScanner scannerWithString:[hex substringWithRange:NSMakeRange(0, 2)]];
-    [scannerRed scanHexInt:&tmpRed];
-    red = (float)tmpRed / 255.0;
+	NSScanner *scannerRed = [NSScanner scannerWithString:[hex substringWithRange:NSMakeRange(0, 2)]];
+	[scannerRed scanHexInt:&tmpRed];
+	red = (float)tmpRed / 255.0;
     
-    NSScanner *scannerGreen = [NSScanner scannerWithString:[hex substringWithRange:NSMakeRange(2, 2)]];
-    [scannerGreen scanHexInt:&tmpGreen];
-    green = (float)tmpGreen / 255.0;
+	NSScanner *scannerGreen = [NSScanner scannerWithString:[hex substringWithRange:NSMakeRange(2, 2)]];
+	[scannerGreen scanHexInt:&tmpGreen];
+	green = (float)tmpGreen / 255.0;
     
-    NSScanner *scannerBlue = [NSScanner scannerWithString:[hex substringWithRange:NSMakeRange(4, 2)]];
-    [scannerBlue scanHexInt:&tmpBlue];    
-    blue = (float)tmpBlue / 255.0;
+	NSScanner *scannerBlue = [NSScanner scannerWithString:[hex substringWithRange:NSMakeRange(4, 2)]];
+	[scannerBlue scanHexInt:&tmpBlue];    
+	blue = (float)tmpBlue / 255.0;
     
 	NSColor *color = [NSColor colorWithCalibratedRed:red green:green blue:blue alpha:1];
 	return color;
 }
 
 - (BOOL)hasConfigureSheet {
-    return NO;
+	return NO;
 }
 
 - (NSWindow*)configureSheet {
-    return nil;
+	return nil;
 }
 
 @end
